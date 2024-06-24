fd21e8fd37e709c7d96c940adf65a4f6a013c8db
alarv <alarvfm@gmail.com>
2024-06-24 13:52:09 +0300

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,153 @@
+import Card from "@/components/home/card";
+import { DEPLOY_URL } from "@/lib/constants";
+import { Github, Twitter } from "@/components/shared/icons";
+import WebVitals from "@/components/home/web-vitals";
+import ComponentGrid from "@/components/home/component-grid";
+import Image from "next/image";
+import { nFormatter } from "@/lib/utils";
+import {ArrowUpTrayIcon, RocketLaunchIcon} from "@heroicons/react/24/solid";
+
+export default async function Home() {
+  const { stargazers_count: stars } = await fetch(
+    "https://api.github.com/repos/ntua-unit-of-control-and-informatics/jaqpotpy",
+    {
+      ...(process.env.GITHUB_OAUTH_TOKEN && {
+        headers: {
+          Authorization: `Bearer ${process.env.GITHUB_OAUTH_TOKEN}`,
+          "Content-Type": "application/json",
+        },
+      }),
+      // data will revalidate every 24 hours
+      next: { revalidate: 86400 },
+    },
+  )
+    .then((res) => res.json())
+    .catch((e) => console.log(e));
+
+  return (
+    <>
+      <div className="z-10 w-full max-w-2xl px-5 xl:px-0">
+        <a
+            // TODO fix url when posted twitter status about release
+          href="https://x.com/upci_ntua"
+          target="_blank"
+          rel="noreferrer"
+          className="mx-auto mb-5 flex max-w-fit animate-fade-up items-center justify-center space-x-2 overflow-hidden rounded-full bg-blue-100 px-7 py-2 transition-colors hover:bg-blue-200"
+        >
+          <Twitter className="h-5 w-5 text-[#1d9bf0]" />
+          <p className="text-sm font-semibold text-[#1d9bf0]">
+            Introducing Jaqpot
+          </p>
+        </a>
+        <h1
+          className="animate-fade-up bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-4xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm [text-wrap:balance] md:text-7xl md:leading-[5rem]"
+          style={{ animationDelay: "0.15s", animationFillMode: "forwards" }}
+        >
+          Build. Train. Deploy
+        </h1>
+        <p
+          className="mt-6 animate-fade-up text-center text-gray-500 opacity-0 [text-wrap:balance] md:text-xl"
+          style={{ animationDelay: "0.25s", animationFillMode: "forwards" }}
+        >
+          Jaqpot empowers data scientists and developers to seamlessly create, upload, and manage machine learning models using Python or R.
+        </p>
+        <div
+          className="mx-auto mt-6 flex animate-fade-up items-center justify-center space-x-5 opacity-0"
+          style={{ animationDelay: "0.3s", animationFillMode: "forwards" }}
+        >
+          <a
+            className="group flex max-w-fit items-center justify-center space-x-2 rounded-full border border-black bg-black px-5 py-2 text-sm text-white transition-colors hover:bg-white hover:text-black"
+            href={DEPLOY_URL}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <RocketLaunchIcon className="w-4 h-4 group-hover:text-black"/>
+            <p>Deploy your model</p>
+          </a>
+          <a
+            className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-5 py-2 text-sm text-gray-600 shadow-md transition-colors hover:border-gray-800"
+            href="https://github.com/ntua-unit-of-control-and-informatics/jaqpotpy"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Github />
+            <p>
+              <span className="hidden sm:inline-block">Star on</span> GitHub{" "}
+              <span className="font-semibold">{nFormatter(stars)}</span>
+            </p>
+          </a>
+        </div>
+      </div>
+      <div className="my-10 grid w-full max-w-screen-xl animate-fade-up grid-cols-1 gap-5 px-5 md:grid-cols-3 xl:px-0">
+        {features.map(({ title, description, demo, large }) => (
+          <Card
+            key={title}
+            title={title}
+            description={description}
+            demo={demo}
+            large={large}
+          />
+        ))}
+      </div>
+    </>
+  );
+}
+
+const features = [
+  {
+    title: "Empower Your Machine Learning Journey",
+    description:
+      "At Jaqpot, we simplify the process of building, training, and deploying machine learning models. " +
+        "Our intuitive platform allows you to focus on your data and algorithms, while we handle the rest. " +
+        "Create powerful models with Python or R, upload them to Jaqpot, and make them accessible online and through our API.",
+    large: true,
+  },
+  {
+    title: "Flexible Access and Permissions",
+    description:
+      "Manage who can see and use your models with our flexible permissions system. Keep your models private, share them with specific organizations, or make them public for the world to see. With Jaqpot, you have full control over your intellectual property.",
+    demo: <WebVitals />,
+  },
+  {
+    title: "Seamless Integration and Deployment",
+    description:
+      "Deploy your models effortlessly with Jaqpot’s user-friendly interface. Our platform integrates seamlessly into your workflow, allowing you to interact with your models programmatically. Whether you’re building a new application or enhancing an existing one, Jaqpot provides the tools you need to succeed",
+    demo: (
+      <a href={DEPLOY_URL}>
+        <Image
+          src="https://vercel.com/button"
+          alt="Deploy with Vercel"
+          width={120}
+          height={30}
+          unoptimized
+        />
+      </a>
+    ),
+  },
+  {
+    title: "Built-in Auth + Database",
+    description:
+      "Precedent comes with authentication and database via [Auth.js](https://authjs.dev/) + [Prisma](https://prisma.io/)",
+    demo: (
+      <div className="flex items-center justify-center space-x-20">
+        <Image alt="Auth.js logo" src="/authjs.webp" width={50} height={50} />
+        <Image alt="Prisma logo" src="/prisma.svg" width={50} height={50} />
+      </div>
+    ),
+  },
+  {
+    title: "Hooks, utilities, and more",
+    description:
+      "Precedent offers a collection of hooks, utilities, and `@vercel/og`",
+    demo: (
+      <div className="grid grid-flow-col grid-rows-3 gap-10 p-10">
+        <span className="font-mono font-semibold">useIntersectionObserver</span>
+        <span className="font-mono font-semibold">useLocalStorage</span>
+        <span className="font-mono font-semibold">useScroll</span>
+        <span className="font-mono font-semibold">nFormatter</span>
+        <span className="font-mono font-semibold">capitalize</span>
+        <span className="font-mono font-semibold">truncate</span>
+      </div>
+    ),
+  },
+];
