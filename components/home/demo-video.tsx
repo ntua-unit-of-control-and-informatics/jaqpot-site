07d99ffbea021d602def874903088712fab43067
alarv <alarvfm@gmail.com>
2024-07-16 21:15:29 +0300

    fix: add pointer-events none to video

--- components/home/demo-video.tsx
+++ components/home/demo-video.tsx
@@ -1,10 +1,21 @@
 export function DemoVideo() {
-    return (
-        <video width="600" height="340" className="border-none" controls preload="none" autoPlay loop muted playsInline style={{
-            clipPath: 'inset(3px)'
-        }}>
-            <source src="/create-prediction.mp4" type="video/mp4" />
-            Your browser does not support the video tag.
-        </video>
-    )
+  return (
+    <video
+      width="600"
+      height="340"
+      className="pointer-events-none border-none	"
+      controls
+      preload="none"
+      autoPlay
+      loop
+      muted
+      playsInline
+      style={{
+        clipPath: "inset(3px)",
+      }}
+    >
+      <source src="/create-prediction.mp4" type="video/mp4" />
+      Your browser does not support the video tag.
+    </video>
+  );
 }
