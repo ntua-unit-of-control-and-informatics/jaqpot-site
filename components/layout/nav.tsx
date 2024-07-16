ac78f478ebbf2b22b80d41454efbab2086aa2c56
alarv <alarvfm@gmail.com>
2024-07-16 13:18:13 +0300

    fix: fix dependencies and move to npm

--- components/layout/nav.tsx
+++ components/layout/nav.tsx
@@ -1,7 +1,8 @@
 import Navbar from "./navbar";
 import { useSession } from "next-auth/react";
+import { auth } from "@/auth";
 
 export default async function Nav() {
-  const { data: session } = useSession();
+  const session = await auth();
   return <Navbar session={session} />;
 }
