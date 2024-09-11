088a850993493adf07f15439fda1e1af20ffc36a
Alex Arvanitidis <alarvfm@gmail.com>
2024-09-11 11:12:20 +0300

    feat: add opengraph data (#7)

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,5 +1,12 @@
 import Blog from "@/app/blog/components/Blog";
 import { getPosts } from "@/app/blog/posts";
+import { Metadata } from "next";
+import { generateSharedMetadata } from "@/app/shared.metadata";
+
+export const metadata: Metadata = generateSharedMetadata(
+  "Blog",
+  "Blog posts about Jaqpot and its features",
+);
 
 export default async function Page() {
   const posts = await getPosts();
