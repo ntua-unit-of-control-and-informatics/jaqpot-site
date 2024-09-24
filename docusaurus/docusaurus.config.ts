4d5d088a54ec87ee0daca5a9589c8335ce84524c
alarv <alarvfm@gmail.com>
2024-09-24 10:07:48 +0300

    deploy: move to jaqpot.org

--- docusaurus/docusaurus.config.ts
+++ docusaurus/docusaurus.config.ts
@@ -1,49 +1,49 @@
-import { themes as prismThemes } from "prism-react-renderer";
-import type { Config } from "@docusaurus/types";
-import type * as Preset from "@docusaurus/preset-classic";
+import { themes as prismThemes } from 'prism-react-renderer';
+import type { Config } from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: "Jaqpot",
-  tagline: "Build. Train. Deploy.",
-  favicon: "img/favicon.ico",
+  title: 'Jaqpot',
+  tagline: 'Build. Train. Deploy.',
+  favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: "https://jaqpot.org",
+  url: 'https://jaqpot.org',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/docs",
+  baseUrl: '/docs',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "ntua-unit-of-control-and-informatics", // Usually your GitHub org/user name.
-  projectName: "jaqpot", // Usually your repo name.
+  organizationName: 'ntua-unit-of-control-and-informatics', // Usually your GitHub org/user name.
+  projectName: 'jaqpot', // Usually your repo name.
 
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: "en",
-    locales: ["en"],
+    defaultLocale: 'en',
+    locales: ['en'],
   },
 
   presets: [
     [
-      "classic",
+      'classic',
       {
         docs: {
-          routeBasePath: "/", // Serve the docs at the site's root
-          sidebarPath: "./sidebars.ts",
+          routeBasePath: '/', // Serve the docs at the site's root
+          sidebarPath: './sidebars.ts',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: false,
         theme: {
-          customCss: "./src/css/custom.css",
+          customCss: './src/css/custom.css',
         },
       } satisfies Preset.Options,
     ],
@@ -51,68 +51,68 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: "img/opengraph-image.png",
+    image: 'img/opengraph-image.png',
     navbar: {
-      title: "Jaqpot docs",
+      title: 'Jaqpot docs',
       logo: {
-        alt: "Jaqpot Logo",
-        src: "img/jaqpot-j-logo.svg",
+        alt: 'Jaqpot Logo',
+        src: 'img/jaqpot-j-logo.svg',
       },
       items: [
         {
-          type: "docSidebar",
-          sidebarId: "tutorialSidebar",
-          position: "left",
-          label: "Tutorial",
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'Tutorial',
         },
         {
-          href: "https://appv2.jaqpot.org",
-          label: "Home",
-          position: "right",
-          target: "_self",
+          href: 'https://jaqpot.org',
+          label: 'Home',
+          position: 'right',
+          target: '_self',
         },
         {
-          href: "https://github.com/ntua-unit-of-control-and-informatics/jaqpotpy",
-          label: "GitHub",
-          position: "right",
+          href: 'https://github.com/ntua-unit-of-control-and-informatics/jaqpotpy',
+          label: 'GitHub',
+          position: 'right',
         },
       ],
     },
     footer: {
-      style: "dark",
+      style: 'dark',
       links: [
         {
-          title: "Docs",
+          title: 'Docs',
           items: [
             {
-              label: "Tutorial",
-              to: "/",
+              label: 'Tutorial',
+              to: '/',
             },
           ],
         },
         {
-          title: "Community",
+          title: 'Community',
           items: [
             {
-              label: "Stack Overflow",
-              href: "https://stackoverflow.com/questions/tagged/jaqpot",
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/jaqpot',
             },
             {
-              label: "Discord",
-              href: "https://discordapp.com/invite/docusaurus",
+              label: 'Discord',
+              href: 'https://discordapp.com/invite/docusaurus',
             },
             {
-              label: "Twitter",
-              href: "https://x.com/upci_ntua",
+              label: 'Twitter',
+              href: 'https://x.com/upci_ntua',
             },
           ],
         },
         {
-          title: "More",
+          title: 'More',
           items: [
             {
-              label: "GitHub",
-              href: "https://github.com/ntua-unit-of-control-and-informatics/jaqpotpy",
+              label: 'GitHub',
+              href: 'https://github.com/ntua-unit-of-control-and-informatics/jaqpotpy',
             },
           ],
         },
