ad0683be9d4818e4ada1c7dc9f116c43d9744a46
alarv <alarvfm@gmail.com>
2024-11-08 17:42:51 +0200

    fix: wrong url of getting started

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -24,7 +24,7 @@ export default function NavBar({ session }: { session: Session | null }) {
 
   const navbarLinks = [
     {
-      href: '/docs/category/getting-started',
+      href: '/docs/getting-started',
       text: 'Getting started',
     },
     {
@@ -112,63 +112,6 @@ export default function NavBar({ session }: { session: Session | null }) {
           ))}
         </NavbarMenu>
       </Navbar>
-      {/*<div*/}
-      {/*  className={`fixed top-0 flex w-full justify-center ${*/}
-      {/*    scrolled*/}
-      {/*      ? "border-b border-gray-200 bg-white/50 backdrop-blur-xl"*/}
-      {/*      : "bg-white/0"*/}
-      {/*  } z-30 transition-all`}*/}
-      {/*>*/}
-      {/*  <div className="mx-5 flex h-16 w-full max-w-screen-xl items-center justify-between">*/}
-      {/*    <Link href="/" className="flex items-center font-display text-2xl">*/}
-      {/*      <JLogo />*/}
-      {/*    </Link>*/}
-      {/*    <div className="flex flex-row gap-10">*/}
-      {/*      <Link*/}
-      {/*        href="/getting-started"*/}
-      {/*        className="flex items-center font-display text-2xl hover:underline"*/}
-      {/*      >*/}
-      {/*        Getting started*/}
-      {/*      </Link>*/}
-      {/*      <Link*/}
-      {/*        href="/docs"*/}
-      {/*        className="flex items-center font-display text-2xl hover:underline"*/}
-      {/*      >*/}
-      {/*        Docs*/}
-      {/*      </Link>*/}
-      {/*      <Link*/}
-      {/*        href="/blog"*/}
-      {/*        className="flex items-center font-display text-2xl hover:underline"*/}
-      {/*      >*/}
-      {/*        Blog*/}
-      {/*      </Link>*/}
-      {/*      <Link*/}
-      {/*        href="/contact"*/}
-      {/*        className="flex items-center font-display text-2xl hover:underline"*/}
-      {/*      >*/}
-      {/*        Contact*/}
-      {/*      </Link>*/}
-      {/*      <Link*/}
-      {/*        href="/api"*/}
-      {/*        className="flex items-center font-display text-2xl hover:underline"*/}
-      {/*      >*/}
-      {/*        API*/}
-      {/*      </Link>*/}
-      {/*    </div>*/}
-      {/*    <div>*/}
-      {/*      {session ? (*/}
-      {/*        <UserDropdown session={session} />*/}
-      {/*      ) : (*/}
-      {/*        <button*/}
-      {/*          className="rounded-full border border-black bg-black p-1.5 px-4 text-sm text-white transition-all hover:bg-white hover:text-black"*/}
-      {/*          onClick={() => signIn("keycloak")}*/}
-      {/*        >*/}
-      {/*          Sign In*/}
-      {/*        </button>*/}
-      {/*      )}*/}
-      {/*    </div>*/}
-      {/*  </div>*/}
-      {/*</div>*/}
     </>
   );
 }
