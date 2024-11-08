540809b4c8529481562ec65a54d716d8cb4801d5
alarv <alarvfm@gmail.com>
2024-11-08 16:42:50 +0200

    chore: remove openapi script

--- etc/openapi-generate.sh
+++
@@ -1,5 +0,0 @@
- openapi-generator-cli generate \
-    -i ../jaqpot-api/src/main/resources/openapi.yaml \
-    -g markdown \
-    -o docusaurus/docs/jaqpot-api/documentation/full-api-reference
-    --additional-properties disallowAdditionalPropertiesIfNotPresent=true
