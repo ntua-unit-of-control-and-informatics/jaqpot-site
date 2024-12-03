9be99155c74b7b64f46652a7bcae7fad6fac91e8
alarv <alarvfm@gmail.com>
2024-12-03 14:53:10 +0200

    fix: set metadatabase

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,24 @@
-import "./globals.css";
-import Nav from "@/components/layout/nav";
-import Footer from "@/components/layout/footer";
-import { Suspense } from "react";
-import GoogleAnalytics from "@/components/shared/GoogleAnalytics";
-import { Providers } from "@/providers";
-import localFont from "next/font/local";
-import { Inter } from "next/font/google";
-import { auth } from "@/auth";
-import { SessionProvider } from "next-auth/react";
+import './globals.css';
+import Nav from '@/components/layout/nav';
+import Footer from '@/components/layout/footer';
+import { Suspense } from 'react';
+import GoogleAnalytics from '@/components/shared/GoogleAnalytics';
+import { Providers } from '@/providers';
+import localFont from 'next/font/local';
+import { Inter } from 'next/font/google';
+import { auth } from '@/auth';
+import { SessionProvider } from 'next-auth/react';
 
 export const metadata = {
-  title: "Jaqpot - Create, upload, and deploy Machine Learning models",
+  title: 'Jaqpot - Create, upload, and deploy Machine Learning models',
   description: `Jaqpot is a versatile platform for creating, uploading, and deploying machine learning models using Python. Manage model permissions and access them through an intuitive interface or programmatically via API. Discover the power of Jaqpot at ${process.env.NEXT_PUBLIC_APP_URL}.`,
+  metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL!),
 };
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 const sfPro = localFont({
-  src: "./fonts/SF-Pro-Display-Medium.otf",
-  variable: "--font-sf",
+  src: './fonts/SF-Pro-Display-Medium.otf',
+  variable: '--font-sf',
 });
 
 export default async function RootLayout({
--- lib/constants.ts
+++
@@ -1,1 +0,0 @@
-export const DEPLOY_URL = 'https://app.jaqpot.org/dashboard/new/model';
